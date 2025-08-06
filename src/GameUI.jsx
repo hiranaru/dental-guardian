5426029729f21ee3d47fa3f2f8a07d0b822a36be
hiranaru <n-hirano@sbh-sendai.com>
2025-08-06 19:34:03 +0900

    Update GameUI.jsx

--- src/GameUI.jsx
+++ src/GameUI.jsx
@@ -3,13 +3,25 @@ import './GameUI.css'
 export default function GameUI({ score, life, bombs, onPause }) {
   return (
     <div className="game-ui">
-      <div className="ui-top">
-        <div className="score">クリーンポイント：{score.toLocaleString('ja-JP', { minimumIntegerDigits: 9 })}</div>
-        <div className="life">🦷 歯の健康度：{life}</div>
-        <div className="bombs">💣 ボム：{bombs}</div>
+      {/* 上部バー */}
+      <div className="ui-top-bar">
+        <div className="score">
+          クリーンポイント：
+          {score.toLocaleString('ja-JP', { minimumIntegerDigits: 9 })}
+        </div>
+        <div className="life">
+          🦷 歯の健康度：{life}
+        </div>
+        <div className="bombs">
+          💣 ボム：{bombs}
+        </div>
         <button className="pause-button" onClick={onPause}>⏸ ポーズ</button>
       </div>
-      {/* ナビウィンドウやその他UIは後で追加 */}
+
+      {/* 今後追加予定のナビウィンドウやボタン類をここに */}
+      <div className="ui-bottom-bar">
+        {/* ナビウィンドウ、リトライ、タイトルボタンなど予定地 */}
+      </div>
     </div>
-  )
+  );
 }
